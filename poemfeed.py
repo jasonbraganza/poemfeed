3f9ff6908bb4ab621ac9d4b7416c9c9d4f53c729
Mario Jason Braganza <jason@janusworx.com>
2020-11-11 19:11:08 +0530

    Done!
    Signed-off-by: Mario Jason Braganza <jason@janusworx.com>

--- poemfeed.py
+++ poemfeed.py
@@ -1,10 +1,10 @@
 """
 The idea, is for me to crawl the poetry foundation’s, poem of the day page at
 https://www.poetryfoundation.org/poems/poem-of-the-day and put it into an rss
-file for me, so that i can read it in my RSS reader
+file, so that i can read it in my RSS reader
 """
 
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 
 import requests
 from bs4 import BeautifulSoup
@@ -12,30 +12,51 @@ import PyRSS2Gen
 
 
 def main():
+    """
+    Takes the Poetry foundation daily poem link, gets an updated link if a new poem is posted and then creates an rss feed file.
+    """
     poempage = "https://www.poetryfoundation.org/poems/poem-of-the-day"
     reply = get_page_from_pf(poempage)
-    if reply == poempage:
-        create_rss_feed(poempage)
+    if reply:
+        create_rss_feed(reply)
 
 
 def get_page_from_pf(link):
+    """
+    Parses the PF daily poem link page. If it is updated, then it goes and fetches the link to the actual poem page and returns it
+
+    :param link: url
+    :type link: string
+    :return: url
+    :rtype: string
+    """
     rawhtml = requests.get(link)
     soup = BeautifulSoup(rawhtml.text, "html.parser")
 
-    date_of_poem_str = soup.find("meta", {"name": "dcterms.Date"}).get("content")
+    look_full_poem_url = soup.find_all("a", string="Read More")
+    get_first_element_from_poem_url = look_full_poem_url[0]
+    full_poem_url = get_first_element_from_poem_url.attrs["href"]
 
+    date_of_poem_str = soup.find("meta", {"name": "dcterms.Date"}).get("content")
     date_of_poem = datetime.strptime(date_of_poem_str, "%Y-%m-%d")
     date_of_poem = date_of_poem.date()
     today_date = date.today()
-    return link
 
-    # if date_of_poem == today_date:
-    #     return link
-    # else:
-    #     return "No new poem yet!"
+    if date_of_poem == today_date:
+        return full_poem_url
+    elif date_of_poem == today_date - timedelta(days=1):
+        return full_poem_url
+    else:
+        return None
 
 
 def create_rss_feed(poemlink):
+    """
+    Takes the link that we fetched and then writes it to an xml file for a feed reader to fetch
+
+    :param poemlink: url
+    :type poemlink: string
+    """
     rss = PyRSS2Gen.RSS2(
         title="Jason's PF feed",
         link=poemlink,
